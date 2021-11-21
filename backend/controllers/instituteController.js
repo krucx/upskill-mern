39e84d8cd58182fb7d71e7edf138505b48909455
kritarth <kritarth.jain11@gmail.com>
2021-11-21 15:29:32 +0530

    Institute CRUD ops done

--- backend/controllers/instituteController.js
+++ backend/controllers/instituteController.js
@@ -8,6 +8,61 @@ exports.createInstitute = async (req,res,next)=>{
     });
 }
 
-exports.getAllInstitutes = (req,res)=>{
-    res.status(200).json({message:"Route Configured correctly!!"})
+exports.getAllInstitutes = async (req,res)=>{
+
+    const institutes = await Institute.find();
+    res.status(200).json({
+        success:true,
+        institutes
+    });
+}
+
+exports.updateInstitute = async (req,res)=>{
+    let institute = await Institute.findById(req.params.id);
+
+    if(!institute){
+        return res.status(500).json({
+            success:false,
+            message:"Institute not found"
+        })
+    }
+
+    institute = await Institute.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true,useFindAndModify:false});
+    res.status(200).json({
+        success:true,
+        institute
+    })
+}
+
+exports.deleteInstitute = async (req,res,next) => {
+    let institute = await Institute.findById(req.params.id);
+
+    if(!institute){
+        return res.status(500).json({
+            success:false,
+            message:"Institute not found"
+        })
+    }
+
+    await institute.deleteOne();
+    res.status(200).json({
+        success:true,
+        message:"Institute removed successfully"
+    })
+}
+
+exports.getInstitute = async (req,res,next)=>{
+    let institute = await Institute.findById(req.params.id);
+
+    if(!institute){
+        return res.status(500).json({
+            success:false,
+            message:"Institute not found"
+        })
+    }
+
+    res.status(200).json({
+        success:true,
+        institute
+    })
 }
