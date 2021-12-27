e9562d41ad0937e592312ca52d113ca06414985a
kritarth <kritarth.jain11@gmail.com>
2021-12-27 23:04:01 +0530

    Added post user and profile models

---
+++ models/Blog.js
@@ -0,0 +1,73 @@
+const mongoose = require("mongoose");
+
+const BlogSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  photo: {
+    type: Buffer,
+    required: true,
+  },
+  category: {
+    type: String,
+  },
+  tags: {
+    type: [String],
+  },
+  fees: {
+    type: Number,
+  },
+  likes: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Blog = mongoose.model("blog", BlogSchema);
+
+module.exports = Blog;
---
+++ models/Profile.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  company: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  skills: {
+    type: [String],
+    required: true,
+  },
+  bio: {
+    type: String,
+  },
+  social: {
+    youtube: {
+      type: String,
+    },
+    github: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    linkedin: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Profile = mongoose.model("profile", ProfileSchema);
+
+module.exports = Profile;
